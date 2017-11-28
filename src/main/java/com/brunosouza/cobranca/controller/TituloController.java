33731f4b419c230a6ee011679573b3dfc18cfbe0
xbrucce <bruno.rodrigues@venturus.org.br>
2017-11-28 11:42:42 -0200

    Montando o combo status dinamicamente

--- src/main/java/com/brunosouza/cobranca/controller/TituloController.java
+++ src/main/java/com/brunosouza/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.brunosouza.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.brunosouza.cobranca.model.StatusTitulo;
 import com.brunosouza.cobranca.model.Titulo;
 import com.brunosouza.cobranca.repository.TituloRepository;
 
@@ -17,8 +22,9 @@ public class TituloController {
 	private TituloRepository tituloRepository;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -28,6 +34,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }

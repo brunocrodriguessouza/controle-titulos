0f34ac15a43c35bd98ea2fed6df84af04a256cea
xbrucce <bruno.rodrigues@venturus.org.br>
2017-11-29 12:46:31 -0200

    Alterando o H2 pelo banco MySQL

--- src/main/java/com/brunosouza/cobranca/CobrancaApplication.java
+++ src/main/java/com/brunosouza/cobranca/CobrancaApplication.java
@@ -5,12 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.brunosouza.cobranca.repository")
 public class CobrancaApplication {
 
 	public static void main(String[] args) {

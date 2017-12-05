6379096a0e03ca51677d5d7427bbdaca418ddafa
xbrucce <bruno.rodrigues@venturus.org.br>
2017-12-05 13:10:58 -0200

    Configurando o Context path da aplicacao para um Tomcat externo

--- src/main/java/com/brunosouza/cobranca/CobrancaApplication.java
+++ src/main/java/com/brunosouza/cobranca/CobrancaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -18,5 +20,13 @@ public class CobrancaApplication {
 	@Bean
 	public LocaleResolver localResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	public static class MVCConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
 	}
 }
